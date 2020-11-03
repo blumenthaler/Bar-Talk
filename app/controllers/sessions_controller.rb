433826a51c87058193b1836b481ac016636cbaca
blumenthaler <blumenthaleralex@gmail.com>
2020-11-03 14:02:10 -0500

    added flash validation for login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,15 @@ class SessionsController < ApplicationController
     end
 
     def create
+        binding.pry
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:message] = "Invalid Username/Password"
+            redirect_to login_path
+        end 
     end
 
     def destroy
