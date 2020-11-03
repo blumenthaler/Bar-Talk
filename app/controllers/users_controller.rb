a18c6672d2142a9c3faba10e587f71a89b7fc0a6
blumenthaler <blumenthaleralex@gmail.com>
2020-11-03 12:43:22 -0500

    signup flow finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,21 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        @user = User.create(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
     end
 end
