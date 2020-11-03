4ad5ab0f36aa69f687c70e7e6bc1b778cef27bec
blumenthaler <blumenthaleralex@gmail.com>
2020-11-03 15:07:52 -0500

    basic flow for creating recipe; need to abstract name method for recipe/cocktail

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,5 +6,16 @@ class RecipesController < ApplicationController
     end
 
     def create
+        @recipe = Recipe.new(recipe_params)
+        @recipe.user = User.find_by(id: session[:user_id])
+        @recipe.cocktail = Cocktail.find_or_create_by(name: @recipe.name)
+        @recipe.save
+        redirect_to cocktail_path(@recipe.cocktail)
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :ingredients, :garnish, :notes)
     end
 end
