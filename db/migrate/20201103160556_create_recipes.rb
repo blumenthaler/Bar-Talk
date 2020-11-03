3c40db333f723478480c69479989e1d2b3e58ef7
blumenthaler <blumenthaleralex@gmail.com>
2020-11-03 11:09:32 -0500

    generated recipes resource

---
+++ db/migrate/20201103160556_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.string :ingredients
+      t.string :garnish
+      t.text :notes
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :cocktail, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
