f0a36264b5b8c4f94cecc0437cb5e3e411a35de4
blumenthaler <blumenthaleralex@gmail.com>
2020-11-03 15:25:30 -0500

    basic logic for cocktail index

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,9 @@
 class CocktailsController < ApplicationController
+
+    def index
+        @cocktails = Cocktail.all
+    end
+    
     def show
         @cocktail = Cocktail.find_by(id: params[:id])
     end
