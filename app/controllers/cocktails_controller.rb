b42675537647d65669d577ca63ad0d90e775efd6
blumenthaler <blumenthaleralex@gmail.com>
2020-11-06 16:45:21 -0500

    updated cocktails#show for popular recipes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,6 +13,7 @@ class CocktailsController < ApplicationController
             @cocktail = Cocktail.find_by(id: params[:id])
             @user_recipes = @cocktail.recipes.all_by_user(current_user)
             @other_recipes = @cocktail.recipes.all_except_user(current_user)
+            @popular = highest_rated_recipes
         end
     end
 
